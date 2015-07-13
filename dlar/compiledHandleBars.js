d90f8a475d811affcb78d3f69d45304d7c4819b9
danielfung <daniel.fung122@yahoo.com>
2015-07-13 18:49:43 -0400

    fix dlar compiledHandleBars

--- dlar/compiledHandleBars.js
+++ dlar/compiledHandleBars.js
@@ -9,7 +9,7 @@ module.exports = function(testData, preTemp) {
   	compiledScript = "?'Error: JSON missing protocolNumber'";
   }
   else{
-    
+    /*
     var data = fs.readFileSync('./data/15-00143_sql_populatedDLAR.json', {encoding:'utf8'});
     //var data = fs.readFileSync('./data/15-00093_sql_populatedDLAR.json', {encoding:'utf8'});
     testData = JSON.parse(data);
@@ -23,6 +23,6 @@ module.exports = function(testData, preTemp) {
     */
     var compiledScript = preTemp(testData);
   }
-  fs.writeFileSync('./output/test-outDLAR', compiledScript, {encoding:'utf8'});
+  //fs.writeFileSync('./output/test-outDLAR', compiledScript, {encoding:'utf8'});
   return compiledScript;
 };
